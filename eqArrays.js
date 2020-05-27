956e898a301baa25ddae6e7d9426813373a5f4b4
Jehanne Hodge <samarahjhodge@gmail.com>
2020-05-27 00:15:44 +0000

    add equal arrays function

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function that takes in 2 arrays and returns true or false, based on a perfect match
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true
+};
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should pass ??
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should pass
