8a1c0dad016e1b6b7e597e5b526fe725a5fbf1d0
Jehanne Hodge <samarahjhodge@gmail.com>
2020-05-27 18:24:12 +0000

    add middle function to get middle number(s) of array

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,10 @@ const assertEqual = function(actual, expected) {
 //function that takes in 2 arrays and returns true or false, based on a perfect match
 
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i <= array1.length; i++) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
--- middle.js
+++ middle.js
@@ -1,5 +1,8 @@
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i <= array1.length; i++) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
@@ -15,3 +18,26 @@ const assertArraysEqual = function(actual, expected) {
   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const middle = function(array) {
+  if (array.length < 3) {
+    return [];
+
+  } else if (array.length % 2 === 0) {
+    let firstIndex = (array.length / 2) - 1;
+    let secondIndex = (array.length /2);
+    return [array[firstIndex], array[secondIndex]];
+    
+  } else if (array.length % 2 !== 0) {
+    let middleIndex = Math.floor(array.length / 2);
+    return [array[middleIndex]];
+  }
+}
+//Test code
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
