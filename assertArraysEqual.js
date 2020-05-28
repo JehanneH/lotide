d426dfd2341ff2ed18b9f6d48988204fbf7f213e
Jehanne Hodge <samarahjhodge@gmail.com>
2020-05-28 22:39:22 +0000

    add function takeUntil, and minor error fixes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,20 @@
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    return false
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  return true
+  return true;
 };
+
 const assertArraysEqual = function(actual, expected) {
   let equal = eqArrays(actual, expected);
   if (equal) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
---
+++ takeUntil.js
@@ -0,0 +1,51 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let equal = eqArrays(actual, expected);
+  if (equal) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    //console.log(item);
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      break;
+    }
+  }
+  return results;
+};
+
+//test code
+
+// only prints the numbers until -1 (less than 0) -> [1, 2, 5, 7, 2]
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+//prints everything before the comma
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+//test code with assertArraysEqual
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
